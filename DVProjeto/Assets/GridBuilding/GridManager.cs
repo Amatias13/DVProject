b97c34a165775b484660afb54834aedb4b66edd2
henriqueleote <80213140+henriqueleote@users.noreply.github.com>
2022-06-24 02:20:08 +0100

    SmallHouse, BigHouse e Hospital

--- DVProjeto/Assets/GridBuilding/GridManager.cs
+++ DVProjeto/Assets/GridBuilding/GridManager.cs
@@ -8,41 +8,42 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private GameObject hospitalPrefab;
-    [SerializeField] private GameObject housePrefab;
+    [SerializeField] private GameObject smallHousePrefab;
+    [SerializeField] private GameObject bigHousePrefab;
     [SerializeField] private Transform camera;
     [SerializeField] private int width, height;
-    //[SerializeField] private Text moneyText;
+    [SerializeField] private Text moneyText;
     [SerializeField] private float money;
 
     private GameObject choosen;
+
+
     private Dictionary<Vector2, GameObject> placedList;
     private Dictionary<Vector2, Tile> tiles;
     private int hospitalCost = 250;
     private int houseCost = 100;
-    private DataManager dataManager;
 
     private void Start()
     {
         GenerateGrid();
-        choosen = housePrefab; 
+        choosen = smallHousePrefab;
         placedList = new Dictionary<Vector2, GameObject>();
-        dataManager = gameObject.GetComponent<DataManager>();
-        //moneyText.text = money.ToString();
+        moneyText.text = money.ToString();
     }
 
     public void GenerateGrid()
     {
 
         tiles = new Dictionary<Vector2, Tile>();
-        for(int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                spawnedTile.Init(isOffset, new Vector2(x,y), this);
+                spawnedTile.Init(isOffset, new Vector2(x, y), this);
 
                 tiles[new Vector2(x, y)] = spawnedTile;
             }
@@ -59,37 +60,50 @@ public class GridManager : MonoBehaviour
             if (choosen.CompareTag("Hospital"))
                 if ((money - hospitalCost) >= 0)
                 {
-                    Instantiate(choosen, pos, Quaternion.identity);
-                    placedList[pos] = choosen;
-                    dataManager.SetMap(placedList);
+                    GameObject obj = Instantiate(choosen, pos, Quaternion.identity);
+                    placedList[pos] = obj;
                     money -= hospitalCost;
-                    //moneyText.text = money.ToString();
+                    moneyText.text = money.ToString();
                 }
                 else Debug.Log("Não há dinheiro");
 
-            if (choosen.CompareTag("House"))
+            if (choosen.CompareTag("SmallHouse"))
                 if ((money - houseCost) >= 0)
                 {
-                    Instantiate(choosen, pos, Quaternion.identity);
-                    placedList[pos] = choosen;
-                    dataManager.SetMap(placedList);
+                    GameObject obj = Instantiate(choosen, pos, Quaternion.identity);
+                    placedList[pos] = obj;
                     money -= houseCost;
-                    //moneyText.text = money.ToString();
+                    moneyText.text = money.ToString();
                 }
                 else Debug.Log("Não há dinheiro");
         }
-        else Debug.Log("Espaço já ocupado");
+        else
+        {
+            GameObject temp = placedList[pos];
+            if (temp.CompareTag("SmallHouse"))
+            {
+                if ((money - 300) >= 0)
+                {
+                    Destroy(temp);
+                    GameObject super = bigHousePrefab;
+                    GameObject obj = Instantiate(super, pos, Quaternion.identity);
+                    placedList[pos] = obj;
+                    money -= 300;
+                    moneyText.text = money.ToString();
+                }
+            }
+        }
 
     }
 
     public void setHospital()
     {
-        choosen = hospitalPrefab;         
+        choosen = hospitalPrefab;
     }
 
     public void setHouse()
     {
-        choosen = housePrefab;
+        choosen = smallHousePrefab;
     }
 
     public Dictionary<Vector2, GameObject> GetPlaced()

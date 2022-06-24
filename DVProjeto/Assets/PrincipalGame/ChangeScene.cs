5bf3cef88c155045130115cffd3761c636b69c02
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2022-06-24 11:34:36 +0100

    principal game

--- DVProjeto/Assets/PrincipalGame/ChangeScene.cs
+++ DVProjeto/Assets/PrincipalGame/ChangeScene.cs
@@ -7,7 +7,12 @@ public class ChangeScene : MonoBehaviour
 {
     public void MoveToScene(int sceneID)
     {
-        if(sceneID == -1)
+        var data = PlayerPrefs.GetString("GameData", "{}");
+        GameData gameData = JsonUtility.FromJson<GameData>(data);
+        var json = JsonUtility.ToJson(gameData);
+        PlayerPrefs.SetString("GameData", json);
+
+        if (sceneID == -1)
         {
             int randomNumber = Random.Range(2, 4);
             SceneManager.LoadScene(randomNumber);
